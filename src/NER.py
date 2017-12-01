d1093cc701800462de66352a46eb93382442f8e8
mushroomgenie <mashthegreat@gmail.com>
2017-12-01 18:51:11 +0530

    NER Update

--- src/NER.py
+++ src/NER.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import sys,re
+import sys,re 
 # from HindiTokenizer import Tokenizer
 text= raw_input().decode(sys.stdin.encoding or locale.getpreferredencoding(True))
 # t = Tokenizer(text)
@@ -14,11 +14,12 @@ words = newline.split(u" ")
 
 #newline= re.sub(r'\'', '', line)
 
-newline= re.findall('[a-zA-Z0-9]+', newline)
+newline = re.findall('[a-zA-Z0-9]+', newline)
 final_str = ""
-newline= " ".join(newline)
-final_str+=newline
-final_str+='\n'
+newline = " ".join(newline)
+final_str += newline
+final_str += '\n'
+date_regex = re.compile("^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$")
 # words = t.tokenize()
 # t.print_tokens
 file = open("../Data/tokenized-hindi","r+")
@@ -41,7 +42,7 @@ for line in lines:
 
 # for i in rules.keys():
 	# print(i,rules[i])
-gazette_list = [u"जनवरी",u"फ़रवरी",u"मार्च",u"अप्रैल",u"मई",u"जून",u"जुलाई",u"अगस्त",u"सितंबर",u"अक्टूबर",u"नवंबर",u"दिसंबर"]
+gazetteer_list = [u"जनवरी",u"फ़रवरी",u"मार्च",u"अप्रैल",u"मई",u"जून",u"जुलाई",u"अगस्त",u"सितंबर",u"अक्टूबर",u"नवंबर",u"दिसंबर"]
 op = ""
 for word in range(len(words)):
 	# print(tokens)
@@ -50,15 +51,17 @@ for word in range(len(words)):
 		words[word+2] += "/I-Amendment"
 	elif words[word] == u"अनुच्छेद":
 		pass	
-	elif words[word] == u"धारा":
+	elif words[word] == u"धारा" and word != len(words)-1 and words[word+1].isnumeric():
 		words[word] += "/B-Section"
 		words[word+1] += "/I-Section"
-	elif words[word] == u"द्वारा":
-		words[word+1] += "B-Time"
-	elif words[word] in [gazette_list]:
+	elif words[word] == u"द्वारा" and date_regex.match(words[word+1]):
+		words[word+1] += "/B-Time"
+	elif words[word] in [gazetteer_list] and word != 0 and words[word-1].isnumeric():
 		words[word-1] += "/B-Time"
 		words[word] += "/I-Time"
 		words[word+1] += "/I-Time"
+	elif words[word] == u"अधिनियम" and word != len(words)-1 and words[word+1].isnumeric():
+		words[word+1] += "/B-Time"
 	else:# words[word] in rules.keys():
 		for key in rules.keys():
 			if key.decode('utf-8') == words[word]:
